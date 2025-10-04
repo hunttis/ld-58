50f4d8b106cf2092f1ed7d6501efb2127bdae10b
Joni Katajamäki <ext-joni.katajamaki@vr.fi>
2025-10-04 10:10:40 +0300

    sheep moves!

--- assets/scenes/game/sheep/Sheep.gd
+++ assets/scenes/game/sheep/Sheep.gd
@@ -3,6 +3,7 @@ extends Node3D
 class_name Sheep
 
 @export var character_body: CharacterBody3D
+@export var speed: float = 10
 
 enum State {
   Idle,
@@ -11,7 +12,11 @@ enum State {
 var state: State = State.Idle
 
 func _process(delta: float) -> void:
+  pass
+
+
+func _physics_process(delta: float) -> void:
   match state:
     State.Idle:
-      character_body.velocity += Vector3.FORWARD * 10 * delta
-  pass
+      character_body.velocity += Vector3.FORWARD * speed * delta
+  character_body.move_and_slide()
