f88112ee6a12d52e8eeb9a753c36f6846aaadb3b
Joni Katajamäki <ext-joni.katajamaki@vr.fi>
2025-10-04 10:27:39 +0300

    add 100 sheeps

---
+++ assets/scenes/game.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+const SheepScene = preload("uid://divjv3eaenwq7")
+
+func _ready():
+  for x in range(10):
+    for z in range(10):
+      var sheep = SheepScene.instantiate()
+      sheep.position = Vector3(x * 10 - 50, 0, z * 10 - 50)
+      add_child(sheep)
