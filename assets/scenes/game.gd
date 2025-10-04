b6b9972d949654e703ea0b725d3b852f0b11c046
Joni Katajamäki <ext-joni.katajamaki@vr.fi>
2025-10-04 11:27:28 +0300

    add sheep to group "sheep"

--- assets/scenes/game.gd
+++ assets/scenes/game.gd
@@ -7,4 +7,5 @@ func _ready():
     for z in range(10):
       var sheep = SheepScene.instantiate()
       sheep.position = Vector3(x * 10 - 50, 0, z * 10 - 50)
+      sheep.add_to_group("sheep")
       add_child(sheep)
