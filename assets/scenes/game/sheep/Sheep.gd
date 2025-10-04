c496fa8bb46c010dcf9a92ab77344b4675dd014d
Joni Katajamäki <ext-joni.katajamaki@vr.fi>
2025-10-04 09:45:02 +0300

    feat: add state var to sheep

--- assets/scenes/game/sheep/Sheep.gd
+++ assets/scenes/game/sheep/Sheep.gd
@@ -1,1 +1,15 @@
 extends Node3D
+
+class_name Sheep
+
+enum State {
+  Idle,
+}
+
+var state: State = State.Idle
+
+func _process(delta: float) -> void:
+  match state:
+    State.Idle:
+      pass
+  pass
