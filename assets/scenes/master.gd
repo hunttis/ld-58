5c59b4a110e896df218d12f7c6f8ca5b7ca6a13c
MikkoKarvonen <mkarvonen34@gmail.com>
2025-10-05 13:05:55 +0300

    Add music

--- assets/scenes/master.gd
+++ assets/scenes/master.gd
@@ -4,6 +4,10 @@ var game_scene_file = preload("res://assets/scenes/game.tscn")
 var options_scene_file = preload("res://assets/scenes/menus/options.tscn")
 var main_menu_scene_file = preload("res://assets/scenes/menus/main_menu.tscn")
 
+var current_scene_type: Scenes
+var musicOn = 0
+var musicOff = -80
+
 enum Scenes {
   Main_Menu,
   Options,
@@ -12,6 +16,9 @@ enum Scenes {
 
 @export var default_scene: Scenes = Scenes.Main_Menu
 @onready var current_scene = $CurrentScene
+@onready var titleMusic = $"AudioStreamPlayerTitle"
+@onready var gameMusic = $"AudioStreamPlayerGame"
+
 
 func _ready():
   Events.go_to_game.connect(_on_go_to_game)
@@ -26,6 +33,7 @@ func _ready():
     Scenes.Game:
       _on_go_to_game()
 
+  gameMusic.volume_db = musicOff
 
 func _empty_current_scene():
   for child_scene in current_scene.get_children():
@@ -35,13 +43,34 @@ func _on_go_to_game():
   _empty_current_scene()
   var game_scene = game_scene_file.instantiate()
   current_scene.add_child(game_scene)
+  current_scene_type = Scenes.Game
+  update_music()
 
 func _on_go_to_menu():
   _empty_current_scene()
   var menu_scene = main_menu_scene_file.instantiate()
   current_scene.add_child(menu_scene)
+  current_scene_type = Scenes.Main_Menu
+  update_music()
 
 func _on_go_to_options():
   _empty_current_scene()
   var options_scene = options_scene_file.instantiate()
   current_scene.add_child(options_scene)
+  current_scene_type = Scenes.Options
+  update_music()
+    
+func update_music():
+    match current_scene_type:
+        Scenes.Game:
+            titleMusic.volume_db = musicOff
+            gameMusic.volume_db = musicOn
+        Scenes.Main_Menu:
+            titleMusic.volume_db = musicOn
+            gameMusic.volume_db = musicOff
+        Scenes.Options:
+            titleMusic.volume_db = musicOn
+            gameMusic.volume_db = musicOff
+        _:
+            titleMusic.volume_db = musicOn
+            gameMusic.volume_db = musicOff
--- assets/scenes/menus/options.gd
+++ assets/scenes/menus/options.gd
@@ -1,27 +1,31 @@
 extends Control
 
+
 func _ready() -> void:
-	$OptionsContainer/ButtonContainer/MasterVolumeSlider.value = AudioServer.get_bus_volume_linear(AudioServer.get_bus_index("Master"))
-	$OptionsContainer/ButtonContainer/MusicVolumeSlider.value = AudioServer.get_bus_volume_linear(AudioServer.get_bus_index("Music"))
-	$OptionsContainer/ButtonContainer/SoundVolumeSlider.value = AudioServer.get_bus_volume_linear(AudioServer.get_bus_index("Sound"))
-	$OptionsContainer/ButtonContainer/MasterVolumeSlider.grab_focus()
+    print($OptionsContainer/ButtonContainer/MusicVolumeSlider.value)
+
+    $OptionsContainer/ButtonContainer/MasterVolumeSlider.value = AudioServer.get_bus_volume_linear(AudioServer.get_bus_index("Master"))
+    $OptionsContainer/ButtonContainer/MusicVolumeSlider.value = AudioServer.get_bus_volume_linear(AudioServer.get_bus_index("Music"))
+    $OptionsContainer/ButtonContainer/SoundVolumeSlider.value = AudioServer.get_bus_volume_linear(AudioServer.get_bus_index("Sound"))
+    $OptionsContainer/ButtonContainer/MasterVolumeSlider.grab_focus()
 
 func _on_music_button_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db($Slider.value))
-	print("Music mute toggled to: ", !toggled_on)
+    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db($Slider.value))
+    print("Music mute toggled to: ", !toggled_on)
 
 func _on_sound_button_toggled(toggled_on: bool) -> void:
-	AudioServer.set_bus_mute(1, !toggled_on)
-	print("Sound mute toggled to: ", !toggled_on)
+    AudioServer.set_bus_mute(1, !toggled_on)
+    print("Sound mute toggled to: ", !toggled_on)
 
 func _on_master_volume_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
+    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear_to_db(value))
 
 func _on_music_volume_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value))
+    print(value)
+    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Music"), linear_to_db(value))
 
 func _on_sound_volume_slider_value_changed(value: float) -> void:
-	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound"), linear_to_db(value))
+    AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Sound"), linear_to_db(value))
 
 func _on_return_to_menu_button_pressed() -> void:
-	Events.go_to_menu.emit()
+    Events.go_to_menu.emit()
