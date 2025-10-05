91148e233aa63e609cafbed5e70e82ce3394c1aa
Ville Hunttis Nuutinen <hunttis@me.com>
2025-10-05 14:26:53 +0300

    Heart effect cache

--- assets/scenes/game.gd
+++ assets/scenes/game.gd
@@ -1,10 +1,10 @@
 extends Node3D
 
 const SheepScene = preload("uid://divjv3eaenwq7")
+var heart_fx_scene = preload("res://assets/scenes/game/sheep/SheepHeartEffect.tscn")
 
 var total_sheep
 var coralled_sheep: set = _set_coralled_sheep
-var bus = AudioServer.get_bus_index("Music")
 
 @onready var level_timer: Timer = $LevelTimer
 @onready var global_bleat_timer = $GlobalBleatTimer
@@ -12,12 +12,13 @@ var bus = AudioServer.get_bus_index("Music")
 @export var global_bleat_timeout_max: float = 8.0
 @export var global_bleat_timeout_min: float = 0.5
 
-@onready var game_music = $AudioStreamPlayerGame
-
 signal level_complete
 signal level_failed
 
 func _ready():
+  var heart_fx = heart_fx_scene.instantiate()
+  add_child(heart_fx)
+  heart_fx.visible = false
   add_child(level_timer)
   coralled_sheep = 0
 
@@ -33,7 +34,6 @@ func _ready():
 
   level_timer.start()
   global_bleat_timer.start()
-  AudioServer.remove_bus_effect(bus, 0)
 
 func _set_coralled_sheep(new_val: int):
   coralled_sheep = new_val
@@ -48,7 +48,7 @@ func _on_level_timer_timeout():
   AudioServer.add_bus_effect(bus, lowpass, 0)
   level_failed.emit()
   level_timer.stop()
-    
+
 func _on_sheep_coralled():
   coralled_sheep = get_tree().get_nodes_in_group("coralled").size()
   if get_tree().get_nodes_in_group("sheep").size() == 0:
