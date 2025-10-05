72859b07c108c42d033bd119062cc96370ba7867
Juuso Elo-Rauta <juuso.elo-rauta@wunderdog.fi>
2025-10-05 10:03:32 +0300

    remove mouse movement

--- assets/scenes/game/dog/dog.gd
+++ assets/scenes/game/dog/dog.gd
@@ -3,7 +3,6 @@ extends CharacterBody3D
 
 const bark_scene = preload("uid://ywcmei680bhq")
 
-@onready var agent: NavigationAgent3D = $NavigationAgent3D
 @onready var repulsionArea: Area3D = $MoveThreatRange
 @onready var repulsionCollider: CollisionShape3D = $MoveThreatRange/CollisionShape3D
 @onready var barkCooldown: Timer = $BarkCooldown
@@ -64,41 +63,35 @@ func _process(delta):
     cur_stamina = min(cur_stamina + stamina_regen * delta, max_stamina)
 
 func _physics_process(delta: float) -> void:
+  var input_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
+  var move_dir = Vector3(input_dir.x, 0, input_dir.y)
   if move_state != Global.DOG_MOVE_STATE.DASH:
-    var move_dir = Input.get_vector("move_left", "move_right", "move_forward", "move_back")
     if move_dir.length() > 0:
-      agent.target_position = global_position + Vector3(move_dir.x, 0, move_dir.y) * 10
-    if agent.is_navigation_finished():
+      velocity = move_dir * speed
+    else:
       velocity = Vector3.ZERO
-      return
-    var target = agent.get_next_path_position()
-    var direction = global_position.direction_to(target)
-    velocity = direction * speed
-
-    var look_target = position + velocity
-    look_at(Vector3(look_target.x, global_position.y, look_target.z))
-    move_and_slide()
-
+    
     if move_state == Global.DOG_MOVE_STATE.SPRINT:
       cur_stamina = max(cur_stamina - sprint_stamina_cost * delta, 0)
       if cur_stamina <= 0:
         move_state = Global.DOG_MOVE_STATE.NORMAL
+    
+    move_and_slide()
 
   if move_state == Global.DOG_MOVE_STATE.DASH:
-    var dir = global_position.direction_to(dash_target)
-    velocity = dir * dash_speed
+    if dash_target == Vector3.ZERO:
+      dash_target = global_position + (move_dir * dash_distance)
+      
+    velocity = global_position.direction_to(dash_target) * dash_speed
     var collided = move_and_slide()
+    
     if global_position.distance_to(dash_target) <= 1 || collided:
       move_state = Global.DOG_MOVE_STATE.NORMAL
-      agent.target_position = global_position
       velocity = Vector3.ZERO
-
-func _input(_event) -> void:
-  if Input.is_action_just_pressed("LeftClick") && move_state != Global.DOG_MOVE_STATE.DASH:
-    var target = get_vector_to_cursor_pos()
-    print('Clicked', target)
-    agent.target_position = target
-
+      dash_target = Vector3.ZERO
+  
+  var look_target = position + velocity
+  look_at(Vector3(look_target.x, global_position.y, look_target.z))
 
 func _unhandled_input(event: InputEvent):
     if event.is_action_pressed("ability_sprint"):
@@ -132,29 +125,7 @@ func _dash():
     return
 
   cur_stamina -= dash_stamina_cost
-  agent.target_position = global_position
-  var dash_dir = global_position.direction_to(get_vector_to_cursor_pos())
-  dash_dir.y = 0
-  dash_target = global_position + (dash_dir * dash_distance)
-  print("Dash target: ", dash_target)
   move_state = Global.DOG_MOVE_STATE.DASH
-
-func get_vector_to_cursor_pos() -> Vector3:
-  var camera := get_tree().get_nodes_in_group("Camera")[0] as Camera3D
-  var click_pos := get_viewport().get_mouse_position()
-  var ray_length := camera.far
-  var origin := camera.project_ray_origin(click_pos)
-  var direction := camera.project_ray_normal(click_pos)
-  var end_position = origin + direction * ray_length
-  var space := get_world_3d().direct_space_state
-  var query := PhysicsRayQueryParameters3D.create(origin, end_position)
-  var result = space.intersect_ray(query)
-  if result.is_empty():
-    printerr("Cursor position returned empty!")
-    return Vector3.ZERO
-
-  return result.position
-
 
 func set_repulsion_range(new_range: float) -> void:
   var shape = repulsionCollider.shape as SphereShape3D
