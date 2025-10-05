a893f244b12f607690eaa6e58fc664ca9de56599
Mika Viljanen <mika.viljanen@wunderdog.fi>
2025-10-05 15:26:32 +0300

    Fix camera tilt

--- assets/scenes/game_camera.gd
+++ assets/scenes/game_camera.gd
@@ -18,22 +18,15 @@ const TILT_MAX: float = 20
 const TILT_MIN: float = 12
 
 var camera_offset: Vector3
+var tilt_axis: Vector3
 
 func _ready() -> void:
   camera_offset = global_position
+  tilt_axis = Vector3.LEFT
   Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 func _process(delta: float) -> void:
-  var dogs = get_tree().get_nodes_in_group("dog") as Array[CharacterBody3D]
-  if dogs.is_empty():
-    return
-  
-  var average_pos = Vector3.ZERO
-  for dog in dogs:
-    average_pos += dog.global_position
-  average_pos /= dogs.size()
-  
-  global_position = average_pos + camera_offset
+  global_position = target.global_position + camera_offset
   
   var rotate_direction = Rotation.NONE
   var tilt_direction = Tilt.NONE
@@ -47,20 +40,24 @@ func _process(delta: float) -> void:
     tilt_direction = Tilt.DOWN
   
   if (rotate_direction != Rotation.NONE):
-    camera_offset = camera_offset.rotated(Vector3.UP, delta * rotate_direction)
+    rotate_camera(delta * rotate_direction)
   if (tilt_direction != Tilt.NONE):
-    tilt_camera(delta * tilt_direction)
+    tilt_camera(delta * tilt_direction / 2)
 
   look_at(target.position)
   target.set_camera_rotation(rotation.y)
 
 func _input(event):
   if event is InputEventMouseMotion:
-    camera_offset = camera_offset.rotated(Vector3.UP, event.relative.x * -0.005)
+    rotate_camera(event.relative.x * -0.005)
     tilt_camera(event.relative.y * -0.001)
 
+func rotate_camera(angle: float):
+    camera_offset = camera_offset.rotated(Vector3.UP, angle)
+    tilt_axis = tilt_axis.rotated(Vector3.UP, angle)
+
 func tilt_camera(angle: float):
-  var new_camera_offset = camera_offset.rotated(Vector3.LEFT, angle)
+  var new_camera_offset = camera_offset.rotated(tilt_axis, angle)
 
   if new_camera_offset.y > TILT_MIN && new_camera_offset.y < TILT_MAX:
    camera_offset = new_camera_offset
